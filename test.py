805d3a7a8a011525939f0bde675d03b74d57c3f9
Tulio Coelho <tulioopc@gmail.com>
2017-10-09 16:03:50 -0300

    Training and testing model without some zeros

--- test.py
+++ test.py
@@ -8,24 +8,21 @@ from sklearn.ensemble import ExtraTreesClassifier
 from termcolor import cprint
 from sklearn.externals import joblib
 
+print('Separando dados para treinamento')
+data_for_train = pd.read_csv('blog_dataset/train_without_some_zeros.csv')
+x_train = np.array(data_for_train.iloc[:, 0:280])
+y_train = np.array(data_for_train.iloc[:, -1])
+cprint('Dados separados com sucesso!', 'green')
 
-print('Lendo dados para Treinamento')
-data_train = pd.read_csv('blog_dataset/train.csv', header=None)
-x_train = np.array(data_train.iloc[0:20000, 0:280])
-y_train = np.array(data_train.iloc[0:20000, -1])
-cprint('Dados lidos com sucesso!', 'green')
-
-print('Criando dados para Teste')
-data_test = pd.read_csv('blog_dataset/test.csv', header=None)
-x_test = np.array(data_test.iloc[:, 0:280])
-y_test = np.array(data_test.iloc[:, -1])
+print('Separando dados para teste!')
+data_for_test = pd.read_csv('blog_dataset/test_without_some_zeros.csv')
+x_test = np.array(data_for_test.iloc[:, 0:280])
+y_test = np.array(data_for_test.iloc[:, -1])
 cprint('Dados para teste criados com sucesso!', 'green')
 
 vt = VarianceThreshold(threshold=(.80 * (1 - .80)))
 x_train_vt = vt.fit_transform(x_train, y_train)
 x_test_vt = vt.fit_transform(x_test, y_test)
-print(x_train.shape)
-print(x_train_vt.shape)
 
 '''
 clf = ExtraTreesClassifier()
@@ -46,13 +43,9 @@ print(x_train_clf.shape)
 
 print('Treinando Modelo')
 regr = DecisionTreeRegressor()
-
 regr.fit(x_train_vt,y_train)
 
 print("Obtendo Resultados")
-y = np.array(y_test)
-x = np.array(x_test)
-
 print(cross_val_score(regr, x_test_vt, y_test, scoring='explained_variance'))
 
 joblib.dump(regr, 'decisionTreeRegressor.pkl')
